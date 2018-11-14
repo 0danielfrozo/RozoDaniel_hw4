10b1b9a5cf78590b8ac3008b6de0a7d743b18a53
0danielfrozo <df.rozo11@uniandes.edu.co>
2018-11-14 01:26:36 -0500

    Se realizo la implentacion del atercera condicion de frontera periodica

--- PED.cpp
+++ PED.cpp
@@ -17,15 +17,15 @@ double pos_varilla[50][50];
 void impresion();
 void varilla(int lon ,double diam);
 void cond_ini_1(int lon, double t_tot, double dt);
+void cond_ini_3(int lon, double t_tot, double dt);
 
 int main(){
 
  double diam=10;
+  
+ datos.open("con_1.txt");
+ 
  varilla(lon,diam);
-
- 
- datos.open("PDE.txt");
- 
  cond_ini_1(lon,0,0.005);
  impresion();
  varilla(lon,diam);
@@ -38,9 +38,24 @@ int main(){
  cond_ini_1(lon,5,0.005);
  impresion();
  
-
  datos.close();
  
+ varilla(lon,diam);
+ datos.open("con_3.txt");
+ cond_ini_3(lon,0,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_3(lon,10,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_3(lon,100,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_3(lon,350,0.005);
+ impresion();
+ 
+ datos.close();
+
  return 0;
 }
 
@@ -101,7 +116,50 @@ void cond_ini_1(int lon, double t_tot, double dt){
  }
 }
 
+void cond_ini_3(int lon, double t_tot, double dt){
+ double h=0.01;
+ double nu=k/(rho*Cp);
  
+ if(dt==0){dt=1;} 
+ double pasos_tiempo=t_tot/dt;
+ double calcita_nuevo[50][50];
+ for(int t=0;t<pasos_tiempo;t++){
+  for(int x=1;x<lon-1;x++){
+   for (int y=1;y<lon-1;y++){
+    if(pos_varilla[x][y]!=1){
+     calcita_nuevo[x][y]=calcita[x][y]+(dt*nu/(h*h))*((calcita[x+1][y]-2*calcita[x][y]+calcita[x-1][y])+(calcita[x][y+1]-2*calcita[x][y]+calcita[x][y-1]));
+    }
+    else{
+     calcita_nuevo[x][y]=calcita[x][y];
+    }
+   }
+  }
+  for( int i=1;i<lon-1;i++){
+   calcita_nuevo[0][i]=calcita[0][i]+(dt*nu/(h*h))*((calcita[0+1][i]-2*calcita[0][i]+calcita[lon-1][i])+(calcita[0][i+1]-2*calcita[0][i]+calcita[0][i-1]));
+   
+   calcita_nuevo[lon-1][i]=calcita[lon-1][i]+(dt*nu/(h*h))*((calcita[0][i]-2*calcita[lon-1][i]+calcita[lon-2][i])+(calcita[lon-1][i+1]-2*calcita[lon-1][i]+calcita[lon-1][i-1])); 
+
+   calcita_nuevo[i][0]=calcita[i][0]+(dt*nu/(h*h))*((calcita[i-1][0]-2*calcita[i][0]+calcita[i+1][0])+(calcita[i][0+1]-2*calcita[i][0]+calcita[i][lon-1])); 
+
+   calcita_nuevo[i][lon-1]=calcita[i][lon-1]+(dt*nu/(h*h))*((calcita[i-1][lon-1]-2*calcita[i][lon-1]+calcita[i+1][lon-1])+(calcita[i][0]-2*calcita[i][lon-1]+calcita[i][lon-2])); 
+  }  
+
+  calcita_nuevo[0][0]=calcita[0][0]+(dt*nu/(h*h))*((calcita[0+1][0]-2*calcita[0][0]+calcita[lon-1][0])+(calcita[0][0+1]-2*calcita[0][0]+calcita[0][lon-1]));
+
+  calcita_nuevo[0][lon-1]=calcita[0][lon-1]+(dt*nu/(h*h))*((calcita[0+1][lon-1]-2*calcita[0][lon-1]+calcita[lon-1][lon-1])+(calcita[0][0]-2*calcita[0][lon-1]+calcita[0][lon-2]));
+
+  calcita_nuevo[lon-1][0]=calcita[lon-1][0]+(dt*nu/(h*h))*((calcita[0][0]-2*calcita[lon-1][0]+calcita[lon-2][0])+(calcita[lon-1][0+1]-2*calcita[lon-1][0]+calcita[lon-1][lon-1]));
+
+  calcita_nuevo[lon-1][lon-1]=calcita[lon-1][lon-1]+(dt*nu/(h*h))*((calcita[0][lon-1]-2*calcita[lon-1][lon-1]+calcita[lon-2][lon-1])+(calcita[lon-1][0]-2*calcita[lon-1][lon-1]+calcita[lon-1][lon-2]));
+
+  for( int x=0;x<lon;x++){
+   for ( int y=0;y<lon;y++){
+    calcita[x][y]=calcita_nuevo[x][y];
+   }
+  }
+ }
+}
+
 
 
 
