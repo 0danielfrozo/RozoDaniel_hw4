ed9be05c8370376f81aafc41c24f96e6ff5889f2
0danielfrozo <df.rozo11@uniandes.edu.co>
2018-11-13 22:26:49 -0500

    Creacion de la malla dde la calcita y la varilla

--- PED.cpp
+++ PED.cpp
@@ -2,10 +2,33 @@
 double k = 1.62;
 double Cp= 820;
 double rho=2.71;
-double T_varilla=10;
+double T_varilla=100;
+
+double *calcita;
 
 int main(){
+double lon=50;
+double diam=10;
+calcita= new double[long][long];
+varilla(lon,diam);
+
 
 
 return 0;
 }
+
+
+
+
+
+void varilla(double lon ,double diam){
+ for( int x=0;x<lon;x++){
+  for ( int y=0;y<lon;y++){
+   if( ((x-lon/2)*(x-lon/2)+(y-lon/2)*(y-lon/2))<diam*diam/4){
+    calcita[x][y]=T_varilla;
+   }
+  }
+ }
+}
+
+
