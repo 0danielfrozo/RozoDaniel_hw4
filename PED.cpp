c6012b8fc09b89e1cc3f58fbfa5a194501d11f47
0danielfrozo <df.rozo11@uniandes.edu.co>
2018-11-15 18:35:27 -0500

    implementacion de la segunda condicion de forntera

--- PED.cpp
+++ PED.cpp
@@ -17,6 +17,7 @@ double pos_varilla[50][50];
 void impresion();
 void varilla(int lon ,double diam);
 void cond_ini_1(int lon, double t_tot, double dt);
+void cond_ini_2(int lon, double t_tot, double dt);
 void cond_ini_3(int lon, double t_tot, double dt);
 
 int main(){
@@ -29,19 +30,39 @@ int main(){
  cond_ini_1(lon,0,0.005);
  impresion();
  varilla(lon,diam);
- cond_ini_1(lon,1,0.005);
- impresion();
- varilla(lon,diam);
- cond_ini_1(lon,3,0.005);
- impresion();
- varilla(lon,diam);
  cond_ini_1(lon,5,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_1(lon,10,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_1(lon,50,0.005);
+ impresion();
+ 
+ datos.close();
+
+  
+ datos.open("con_2.txt");
+ 
+ varilla(lon,diam);
+ cond_ini_2(lon,0,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_2(lon,10,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_2(lon,100,0.005);
+ impresion();
+ varilla(lon,diam);
+ cond_ini_2(lon,350,0.005);
  impresion();
  
  datos.close();
  
- varilla(lon,diam);
+ 
  datos.open("con_3.txt");
+
+ varilla(lon,diam);
  cond_ini_3(lon,0,0.005);
  impresion();
  varilla(lon,diam);
@@ -115,6 +136,54 @@ void cond_ini_1(int lon, double t_tot, double dt){
   }
  }
 }
+
+
+
+void cond_ini_2(int lon, double t_tot, double dt){
+ double h=0.01;
+ double nu=k/(rho*Cp);
+ 
+ if(dt==0){dt=1;} 
+ double pasos_tiempo=t_tot/dt;
+ double calcita_nuevo[50][50];
+ for(int t=0;t<pasos_tiempo;t++){
+  for(int x=1;x<lon-1;x++){
+   for (int y=1;y<lon-1;y++){
+    if(pos_varilla[x][y]!=1){
+     calcita_nuevo[x][y]=calcita[x][y]+(dt*nu/(h*h))*((calcita[x+1][y]-2*calcita[x][y]+calcita[x-1][y])+(calcita[x][y+1]-2*calcita[x][y]+calcita[x][y-1]));
+    }
+    else{
+     calcita_nuevo[x][y]=calcita[x][y];
+    }
+   }
+  }
+  for( int i=1;i<lon-1;i++){
+   calcita_nuevo[0][i]=calcita[1][i];
+   
+   calcita_nuevo[lon-1][i]=calcita[lon-2][i];
+
+   calcita_nuevo[i][0]=calcita[i][1];
+
+   calcita_nuevo[i][lon-1]=calcita[i][lon-2]; 
+  }  
+
+  calcita_nuevo[0][0]=calcita[0][0]+(dt*nu/(h*h))*((2*calcita[0+1][0]-2*calcita[0][0])+(2*calcita[0][0+1]-2*calcita[0][0]));
+
+  calcita_nuevo[0][lon-1]=calcita[0][lon-1]+(dt*nu/(h*h))*((2*calcita[0+1][lon-1]-2*calcita[0][lon-1])+(-2*calcita[0][lon-1]+2*calcita[0][lon-2]));
+
+  calcita_nuevo[lon-1][0]=calcita[lon-1][0]+(dt*nu/(h*h))*((-2*calcita[lon-1][0]+2*calcita[lon-2][0])+(2*calcita[lon-1][0+1]-2*calcita[lon-1][0]));
+
+  calcita_nuevo[lon-1][lon-1]=calcita[lon-1][lon-1]+(dt*nu/(h*h))*((-2*calcita[lon-1][lon-1]+2*calcita[lon-2][lon-1])+(-2*calcita[lon-1][lon-1]+2*calcita[lon-1][lon-2]));
+
+  for( int x=0;x<lon;x++){
+   for ( int y=0;y<lon;y++){
+    calcita[x][y]=calcita_nuevo[x][y];
+   }
+  }
+ }
+}
+
+
 
 void cond_ini_3(int lon, double t_tot, double dt){
  double h=0.01;
