4e4c18e0febd24150488bf2338d719b0b388c27b
0danielfrozo <df.rozo11@uniandes.edu.co>
2018-11-19 00:06:05 -0500

    correcion de errores en el archivo

--- hw4.mk
+++ hw4.mk
@@ -4,14 +4,14 @@ Resultados_hw4.pdf: Resultados_hw4.tex 45grados.jpg grados.jpg cond_1.jpg cond_2
 	pdflatex Resultados_hw4.tex
 	rm *.log
 	rm *.aux
-	clean
+	clear
 45grados.jpg grados.jpg cond_1.jpg cond_2.jpg cond_3.jpg promedio_1.jpg promedio_2.jpg promedio_3.jpg: Plots_hw4.py 45grados.txt grados.txt con_1.txt con_3.txt con_2.txt prom_1.txt prom_2.txt prom_3.txt 
 	python Plots_hw4.py 45grados.txt grados.txt con_1.txt con_3.txt con_2.txt prom_1.txt prom_2.txt prom_3.txt 
 45grados.txt grados.txt: ode.cpp
 	g++ ode.cpp
 	./a.out
-con_1.txt con_3.txt con_2.txt prom_1.txt prom_2.txt prom_3.txt:
-	g++ PED.cpp
+con_1.txt con_3.txt con_2.txt prom_1.txt prom_2.txt prom_3.txt: PDE.cpp
+	g++ PDE.cpp
 	./a.out
 clean:
 	rm *.jpg
