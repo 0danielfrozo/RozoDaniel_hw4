08f1ddc08763df26cb8b6db92a9b057c236568c7
0danielfrozo <df.rozo11@uniandes.edu.co>
2018-11-18 23:40:34 -0500

    Finalizacion de archivo make

--- hw4.mk
+++ hw4.mk
@@ -1,5 +1,25 @@
 archivo: Resultados_hw4.pdf
-	display Resultados_hw4.pdf 
-Resultados_hw4: Resultados_hw4.tex grafica_45grados.jpg grafica_variosgrados.jpg  
+	 xdg-open Resultados_hw4.pdf 
+Resultados_hw4.pdf: Resultados_hw4.tex 45grados.jpg grados.jpg cond_1.jpg cond_2.jpg cond_3.jpg promedio_1.jpg promedio_2.jpg promedio_3.jpg
+	pdflatex Resultados_hw4.tex
+	rm *.log
+	rm *.aux
+	clean
+45grados.jpg grados.jpg cond_1.jpg cond_2.jpg cond_3.jpg promedio_1.jpg promedio_2.jpg promedio_3.jpg: Plots_hw4.py 45grados.txt grados.txt con_1.txt con_3.txt con_2.txt prom_1.txt prom_2.txt prom_3.txt 
+	python Plots_hw4.py 45grados.txt grados.txt con_1.txt con_3.txt con_2.txt prom_1.txt prom_2.txt prom_3.txt 
+45grados.txt grados.txt: ode.cpp
+	g++ ode.cpp
+	./a.out
+con_1.txt con_3.txt con_2.txt prom_1.txt prom_2.txt prom_3.txt:
+	g++ PED.cpp
+	./a.out
+clean:
+	rm *.jpg
+	rm *.txt
+	rm *.pdf
+	rm a.out
+	
+
+
 
 
